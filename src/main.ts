d00e4943e14df7dab24b7a684e73eea93b9f9a11
Yuki xD <pxnditxyr@gmail.com>
2023-11-02 23:14:22 -0400

    Initializing Project Hotel Manager backend 🌟 Rest API | Nest 🦁 Prisma 📐

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core'
+import type { NestExpressApplication } from '@nestjs/platform-express'
+import { AppModule } from './app.module'
+import { Logger, ValidationPipe } from '@nestjs/common'
+
+async function bootstrap() {
+  const logger = new Logger( 'Main' )
+  const app = await NestFactory.create<NestExpressApplication>( AppModule )
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    })
+  )
+  await app.listen( process.env.PORT || 3000 )
+  logger.log( `Application is running on: ${ await app.getUrl() }` )
+}
+bootstrap()
